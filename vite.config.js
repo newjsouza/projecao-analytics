13c990830c231afc9347a63bfca508c0bca087a8
Johnathan <newjsouza@gmail.com>
2025-12-04 14:01:39 +0000

    🚋 Reorganizar estrutura - Mover arquivos para raiz

--- vite.config.js
+++ vite.config.js
@@ -1,18 +1,13 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import path from 'path'
 
 export default defineConfig({
   plugins: [react()],
-  root: path.resolve(__dirname, 'public'),
   server: {
-    port: 8080,
+    port: 8080
   },
   build: {
-    outDir: path.resolve(__dirname, 'dist'),
-    emptyOutDir: true,
-    rollupOptions: {
-      input: path.resolve(__dirname, 'public', 'index.html'),
-    },
-  },
+    outDir: 'dist',
+    emptyOutDir: true
+  }
 })
